00ebe0eb7b17bdf720e4c6a56b47075fe6854c3b
douwehorsthuis <douwehorsthuis@gmail.com>
2021-08-04 10:38:30 -0400

    updating # of trials

--- src/Visual_Oddball_duration/Presentation Files/sequences/MMN_makeSequence.m
+++ src/Visual_Oddball_duration/Presentation Files/sequences/MMN_makeSequence.m
@@ -1,12 +1,14 @@
 
-
+clear variables
 % make a sequence for oddball paradigm with the following rules:
 % -> 1 = standard, 2 = deviant.
 % -> A deviant has to be preceded by at least 2 standards
 % -> standards comprise 85% of the sequence and deviants 15%
 % -> the sequence is pseudorandom otherwise.
-ntrls= 500;
-nblcks= 50;
+txt_file_name = '225_sequence_' ; % name of the text files should end in a underscore
+ntrls= 220; %amount of total trials for 1 block
+nblcks= 50; % amount of files the script makes (need 50 for the experiment)
+
 for b = 1:nblcks
     
     sequence = [];
@@ -26,9 +28,13 @@ for b = 1:nblcks
             
             valid_seq = 1;
             
+            for i= 1:5 %making sure that there is no deviant in the first 5 stim
+            if seq(i) == 2 
+                valid_seq = 0;
+            end
+            end
             
-            
-            for i = 2:length(seq)
+            for i = 2:length(seq) %this loop makes sure that there are at least 2 STD between a DEV
                 if i == 2 
                     if seq(i) == 2 && seq(i-1) == 2
                         valid_seq = 0;
@@ -55,8 +61,8 @@ for b = 1:nblcks
     
     % Print the sequence into a text file
     
-    fname = sprintf('225_sequence_%d.txt',b);
-    fid = fopen(['C:\Users\dohorsth\Dropbox (EinsteinMed)\Ana and Douwe - work\cystinosis paradigms\oddball\Oddball_duration\Presentation Files\sequences\' fname],'w');
+    fname = sprintf([txt_file_name '%d.txt'],b);
+    fid = fopen(['C:\Users\dohorsth\Documents\GitHub\Oddball_experiments\src\Visual_Oddball_duration\Presentation Files\sequences\' fname],'w');
     for n = 1:length(sequence)
         fprintf(fid,'%d\n',sequence(n));
     end
